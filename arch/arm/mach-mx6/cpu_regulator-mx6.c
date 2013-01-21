2b921385a5784918ea19052d3bf0c4c416251d1a
Anson Huang <b20788@freescale.com>
2013-01-21 16:20:56 +0800

    ENGR00241003-1 mx6: need to add delay in LDO voltage setting
    1.LDO ramp up time may be modified by ROM code
    according to fuse setting, cpu freq driver use
    fixed delay time which assume the LDO ramp up time
    is the reset value of ANATOP register, need to set
    it to reset value in regulator init.
    
    2.The regulator set voltage should take care of
    the ramp up time, calculate the ramp up time based
    of register setting and to the delay, make sure that
    when the set voltage function return, the voltage is
    stable enough.
    
    3.CPUFreq no need to use delay, it is already taken
    care by regulator voltage setting.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Acked-by: Lily Zhang
    
    Conflicts:
    	arch/arm/mach-mx6/cpu_regulator-mx6.c
    	arch/arm/mach-mx6/mx6_anatop_regulator.c

--- arch/arm/mach-mx6/cpu_regulator-mx6.c
+++ arch/arm/mach-mx6/cpu_regulator-mx6.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2012-2013 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -37,6 +37,7 @@ static struct cpu_op *cpu_op_tbl;
 extern struct cpu_op *(*get_cpu_op)(int *op);
 
 extern unsigned long loops_per_jiffy;
+int external_pureg;
 
 static inline unsigned long mx6_cpu_jiffies(unsigned long old, u_int div,
 					      u_int mult)
@@ -73,13 +74,34 @@ void mx6_cpu_regulator_init(void)
 		} else {
 			curr_cpu = clk_get_rate(cpu_clk);
 			cpu_op_tbl = get_cpu_op(&cpu_op_nr);
-			/* Set the core to max frequency requested. */
+
+			soc_regulator = regulator_get(NULL, soc_reg_id);
+			if (IS_ERR(soc_regulator))
+				printk(KERN_ERR "%s: failed to get soc regulator\n",
+					__func__);
+			else
+				/* set soc to highest setpoint voltage. */
+				regulator_set_voltage(soc_regulator,
+					      cpu_op_tbl[0].soc_voltage,
+					      cpu_op_tbl[0].soc_voltage);
+
+			pu_regulator = regulator_get(NULL, pu_reg_id);
+			if (IS_ERR(pu_regulator))
+				printk(KERN_ERR "%s: failed to get pu regulator\n",
+					__func__);
+			else
+				/* set pu to higheset setpoint voltage. */
+				regulator_set_voltage(pu_regulator,
+					      cpu_op_tbl[0].pu_voltage,
+					      cpu_op_tbl[0].pu_voltage);
+			/* set the core to higheset setpoint voltage. */
 			regulator_set_voltage(cpu_regulator,
 					      cpu_op_tbl[0].cpu_voltage,
 					      cpu_op_tbl[0].cpu_voltage);
+
 			clk_set_rate(cpu_clk, cpu_op_tbl[0].cpu_rate);
 
-			/*Fix loops-per-jiffy */
+			/* fix loops-per-jiffy */
 #ifdef CONFIG_SMP
 			for_each_online_cpu(cpu)
 				per_cpu(cpu_data, cpu).loops_per_jiffy =
@@ -102,11 +124,24 @@ void mx6_cpu_regulator_init(void)
 #endif
 		}
 	}
-	soc_regulator = regulator_get(NULL, soc_reg_id);
-	if (IS_ERR(soc_regulator))
-		printk(KERN_ERR "%s: failed to get soc regulator\n", __func__);
-	pu_regulator = regulator_get(NULL, pu_reg_id);
-	if (IS_ERR(pu_regulator))
-		printk(KERN_ERR "%s: failed to get pu regulator\n", __func__);
+	/*
+	 * if use ldo bypass and VDDPU_IN is single supplied
+	 * by external pmic, it means VDDPU_IN can be turned off
+	 * if GPU/VPU driver not running.In this case we should set
+	 * external_pureg which can be used in pu_enable/pu_disable of
+	 * arch/arm/mach-mx6/mx6_anatop_regulator.c to
+	 * enable or disable external VDDPU regulator from pmic. But for FSL
+	 * reference boards, VDDSOC_IN connect with VDDPU_IN, so we didn't set
+	 * pu_reg_id to the external pmic regulator supply name in the board
+	 * file. In this case external_pureg should be 0 and can't turn off
+	 * extern pmic regulator, but can turn off VDDPU by internal anatop
+	 * power gate.
+	 *
+	 * if enable internal ldo , external_pureg will be 0, and
+	 * VDDPU can be turned off by internal anatop anatop power gate.
+	 *
+	 */
+	if (!IS_ERR(pu_regulator) && strcmp(pu_reg_id, "cpu_vddgpu"))
+		external_pureg = 1;
 }
 
