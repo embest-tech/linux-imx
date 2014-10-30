731b44093c46f713d0dc89eb6109fc767f811ece
luofuchong <luofc@embest-tech.(none)>
2014-10-30 14:46:10 +0800

    Fixup LCD and HDMI display synchronously problem.

--- arch/arm/mach-mx6/board-mx6q_marsboard.c
+++ arch/arm/mach-mx6/board-mx6q_marsboard.c
@@ -960,8 +960,8 @@ static struct fsl_mxc_hdmi_platform_data hdmi_data = {
 };
 
 static struct fsl_mxc_hdmi_core_platform_data hdmi_core_data = {
-	.ipu_id = 0,
-	.disp_id = 0,
+	.ipu_id = 1,
+	.disp_id = 1,
 };
 
 static struct fsl_mxc_lcd_platform_data lcdif_data = {
@@ -995,11 +995,11 @@ static void ldb_init(void)
 }
 
 static struct fsl_mxc_ldb_platform_data ldb_data = {
-	.ipu_id = 1,
+	.ipu_id = 0,
 	.disp_id = 0,
 	.ext_ref = 1,
 	.mode = LDB_SEP0,
-	.sec_ipu_id = 1,
+	.sec_ipu_id = 0,
 	.sec_disp_id = 1,
 };
 
